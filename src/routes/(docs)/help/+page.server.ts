c47dda83edd5678d39aab0e8a34c9ca645046639
jpaquim <joaopaquim@gmail.com>
2022-11-18 18:09:02 +0000

    Add table of contents to /help

--- src/routes/(docs)/help/+page.server.ts
+++ src/routes/(docs)/help/+page.server.ts
@@ -1,14 +1,37 @@
-import { marked } from 'marked';
+import { marked, Renderer } from 'marked';
 import type { PageServerLoad } from './$types';
+
+const renderer = new Renderer();
+const heading = renderer.heading.bind(renderer);
+
+const headings: { anchor: string; level: number; text: string }[] = [];
+
+renderer.heading = (text, level, raw, slugger) => {
+	const anchor = raw.toLowerCase().replaceAll(/[^\w]+/g, '-');
+	headings.push({
+		anchor,
+		level,
+		text
+	});
+	return heading(text, level, raw, slugger);
+};
 
 export const load: PageServerLoad = async () => {
 	const [{ default: file }, { default: preamble }] = await Promise.all([
 		import('/docs/help.md?raw'),
 		import('/docs/preamble.md?raw')
 	]);
-	const content = marked(file)
+	const content = marked(file, { headerIds: true, renderer })
 		.replaceAll('<pre><code>', '<div class="bauble-placeholder"><div class="script">')
 		.replaceAll('</code></pre>', '</div></div>');
-	// TODO: generate equivalent of pandoc --toc
-	return { content, toc: '', preamble: marked(preamble) };
+	const toc =
+		headings.reduce(
+			(state, { anchor, level, text }) => {
+				const delimiter = level > state.level ? '<ul>' : level < state.level ? '</ul>' : '';
+				const element = `<li><a href="#${anchor}">${text}</a></li>`;
+				return { level, html: `${state.html}${delimiter}${element}` };
+			},
+			{ level: 0, html: '' }
+		).html + '</ul>';
+	return { content, toc, preamble: marked(preamble) };
 };
