8e15ee55203db4760f9260bfd9a222af0d272178
jpaquim <joaopaquim@gmail.com>
2023-04-22 19:42:25 +0100

    Update dependencies, remove submodule

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 
-const config: UserConfig = {
-	plugins: [sveltekit()],
-	server: {
-		fs: {
-			allow: ['bauble']
-		}
-	}
-};
-
-export default config;
+export default defineConfig({
+	plugins: [sveltekit()]
+});
